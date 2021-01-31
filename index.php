482a88e23b4fe38f53462823aad66201e4cbdd58
pakping <pakping-up@hotmail.com>
2021-01-31 12:27:25 +0700

    new

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Index</title>
-	<link href="css/lightbox.css" rel="stylesheet">
+	<link href="dist/css/lightbox.min.css" rel="stylesheet">
+	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
 </head>
 <html>
 
@@ -37,7 +37,7 @@
 					<div class="col-xl-3 col-md-6 mb-4">
 						<div class="card border-0 shadow">
 							<a href="<?php echo $img['img_path']; ?>"  data-lightbox="<?php echo $img['img_id']; ?>"  data-title="<?php echo $img['img_title']; ?>">
-								<img src="<?php echo $img['img_path'] ; ?>" class="card-img-top" alt="..."  height="180" width="30">
+								<img src="<?php echo $img['img_path'] ; ?>" class="card-img-top" alt="...">
 							</a>
 							<div class="card-body text-center">
 								<h5 class="card-title"><?php echo $img['img_title']; ?></h5>
@@ -57,8 +57,9 @@
 	</div>
 	<!-- /.container -->
 
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-	<script src="js/lightbox.js"></script>
+	<script src="https://code.jquery.com/jquery-3.0.1.slim.min.js"></script>
+	<script src="dist/js/lightbox-plus-jquery.js"></script>
+	<script src="dist/js/lightbox.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 
