8dd79807b0d9f0a28fa4341f9ba0ea075835216a
pakping <pakping-up@hotmail.com>
2021-01-31 15:09:01 +0700

    tg

--- index.php
+++ index.php
@@ -6,14 +6,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Index</title>
 	<link href="dist/css/lightbox.min.css" rel="stylesheet">
-
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
 	<style>
-.card-img-top {
-    max-height: 20vh; /*not want to take all vertical space*/
-    object-fit: contain;/*show all image, autosized, no cut, in available space*/
-}
+		.card-img-top {
+			max-height: 20vh;
+			/*not want to take all vertical space*/
+			object-fit: contain;
+			/*show all image, autosized, no cut, in available space*/
+		}
 	</style>
 </head>
 <html>
@@ -27,44 +28,47 @@
 	</header>
 	<!-- Header -->
 	<!-- Page Content -->
-	<div class="container">
-		<div class="row g-2">
 
-			<?php
-			require "db/condb.php";
-			$Squery = "SELECT * FROM tbl_photos ORDER BY img_id DESC";
-			if ($result = mysqli_query($conn, $Squery)) {
-				while ($img = mysqli_fetch_array($result)) {
+				<div class="container">
+					<div class="row g-2">
+						<?php
+						require "db/condb.php";
+						$Squery = "SELECT * FROM tbl_photos ORDER BY img_id DESC";
+						if ($result = mysqli_query($conn, $Squery)) {
+							while ($img = mysqli_fetch_array($result)) {
 
-			?>
-					<!-- Team photo-->
-					<div class="col-xl-3 col-md-6 mb-4">
-						<div class="card border-0 shadow">
-							<a href="<?php echo $img['img_path']; ?>" data-lightbox="<?php echo $img['img_id']; ?>" data-title="<?php echo $img['img_title']; ?>">
-								<img src="<?php echo $img['img_path']; ?>" class="card-img-top" alt="...">
-							</a>
-							<div class="card-body text-center">
-								<h5 class="card-title"><?php echo $img['img_title']; ?></h5>
-								<div class="card-text text-black-50"><?php echo $img['img_name']; ?></div>
-								<a  href="download.php?id=<?=$results['imh_id'];?>" ><button class="btn">download</button></a>
-							</div>
-						</div>
+						?>
+								<!-- Team photo-->
+
+
+								<div class="col-xl-3 col-md-6 mb-4">
+									<div class="card border-0 shadow">
+										<a href="<?php echo $img['img_path']; ?>" data-lightbox="<?php echo $img['img_id']; ?>" data-title="<?php echo $img['img_title']; ?>">
+											<img src="<?php echo $img['img_path']; ?>" class="card-img-top" alt="...">
+										</a>
+										<div class="card-body text-center">
+											<h5 class="card-title"><?php echo $img['img_title']; ?></h5>
+											<div class="card-text text-black-50"><?php echo $img['img_name']; ?></div>
+											<a href="download.php?id=<?= $results['imh_id']; ?>"><button class="btn">download</button></a>
+										</div>
+									</div>
+								</div>
+
+
+						<?php
+							}
+						}
+						?>
 					</div>
-
-			<?php
-				}
-			}
-			?>
-
-		</div>
-		<!-- /.row -->
-	</div>
-	<!-- /.container -->
-
+					<!-- /.row -->
+				</div>
+				<!-- /.container -->
+		
 	<script src="https://code.jquery.com/jquery-3.0.1.slim.min.js"></script>
 	<script src="dist/js/lightbox-plus-jquery.js"></script>
 	<script src="dist/js/lightbox.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+	
 </body>
 
 </html>
